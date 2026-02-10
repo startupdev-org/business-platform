f72f8f145aa327a14f6b592035de0a183f1eb5c2
mariusss11 <mariuscarchilan07@gmail.com>
2026-02-10 22:04:02 +0200

    Add some test and new endpoint

--- src/main/java/com/platform/controller/HealthController.java
+++ src/main/java/com/platform/controller/HealthController.java
@@ -1,12 +1,7 @@
 package com.platform.controller;
 
-import com.platform.dto.auth.LoginRequest;
-import com.platform.dto.auth.LoginResponse;
-import com.platform.dto.auth.RegisterRequest;
 import com.platform.service.AuthService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,7 +12,7 @@ public class HealthController {
 
     private final AuthService authService;
 
-    @GetMapping("/health")
+    @GetMapping()
     public ResponseEntity<String> health() {
         return ResponseEntity.ok("The web service is up and running");
     }
